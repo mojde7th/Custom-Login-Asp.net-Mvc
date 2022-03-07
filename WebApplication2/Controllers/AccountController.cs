69c773962368b7d8679b266fff9117556e2c1c63
98912 <98912@DESKTOP-A33184V>
2022-03-07 20:45:42 +0330

    Add project files.

---
+++ WebApplication2/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication2.Models;
+using System.Web.Security;
+namespace WebApplication2.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Me model)
+        {
+            using (var context=new OfficeEntities())
+            {
+                bool isValid = context.User.Any(x=>x.Username==model.Username && x.Pass==model.Pass);
+                if (isValid)
+                {
+                    FormsAuthentication.SetAuthCookie(model.Username,false);
+                    return RedirectToAction("Index","Employees");
+                }
+                ModelState.AddModelError("", "نام کاربری یا رمزعبور اشتباه است");
+                return View();
+            }
+           
+        }
+
+        public ActionResult Signup()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Signup(User model)
+        {
+            using (var context=new OfficeEntities()) {
+                context.User.Add(model);
+                context.SaveChanges();
+            }
+            return RedirectToAction("Login");
+        }
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Login");
+        }
+    }
+}
---
+++ WebApplication2/Models/Me.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication2.Models
+{
+    public class Me
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Pass { get; set; }
+        //public string CompanyStatus { get; set; }
+        //public string CompanyCode { get; set; }
+        //public string PayrollStatus { get; set; }
+        //public string PayrollCode { get; set; }
+    }
+}
